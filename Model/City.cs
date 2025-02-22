ac2daff86f873eb156c9e322b76f9e26edd5e0e8
Fabiana <fabianamayorga31@gmail.com>
2025-02-21 18:22:42 -0800

    Model done

---
+++ Model/City.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model;
+
+[Keyless]
+[Table("City")]
+public partial class City
+{
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [StringLength(50)]
+    [Unicode(false)]
+    public string Name { get; set; } = null!;
+
+    public int Population { get; set; }
+
+    [Column("CountryID")]
+    public int CountryId { get; set; }
+
+    [ForeignKey("CountryId")]
+    public virtual Country Country { get; set; } = null!;
+}
